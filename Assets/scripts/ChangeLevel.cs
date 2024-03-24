4ec232c56cb5de871f74e3efe754bee701ad3914
Nodeions <59334371+Nodeions@users.noreply.github.com>
2024-03-24 09:55:21 +0100

    first_push

--- Assets/scripts/ChangeLevel.cs
+++ Assets/scripts/ChangeLevel.cs
@@ -3,9 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class ChangeLevel : MonoBehaviour
 {
+<<<<<<< Updated upstream
 
+=======
+>>>>>>> Stashed changes
     public string sceneToLoad;
     public void SwitchToEnd() {
         SceneManager.LoadSceneAsync(sceneToLoad);
